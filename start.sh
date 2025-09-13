cde8eb8189c53ddac4dec81bbd1bcd2ce5f87a42
Rogue1807 <150613271+Ruchir1807@users.noreply.github.com>
2025-09-13 11:01:22 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit  
+
+# Activate virtual environment (optional if Railpack auto-installs deps)
+. venv/bin/activate || true
+
+# Run migrations
+python smartqueue/smartqueue/manage.py migrate --noinput
+
+# Collect static files
+python smartqueue/smartqueue/manage.py collectstatic --noinput
+
+# Start Gunicorn (point to the deepest smartqueue.wsgi)
+gunicorn smartqueue.smartqueue.smartqueue.wsgi:application --bind 0.0.0.0:$PORT
